6e319763f6fb56268e8df9e6fc2a59416f990fad
1Chabot <140531553+1Chabot@users.noreply.github.com>
2023-07-25 16:13:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    type: 'list',
+    message: 'Select a member to add skills to',
+    choices: ['Member 1', 'Member 2', 'Member 3'],
+  };
+}
